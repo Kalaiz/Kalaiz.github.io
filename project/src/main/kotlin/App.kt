8d85aba0b7265ea0ed140d09846274a90f1f2f3e
kalai <ssokalai@gmail.com>
2020-12-07 03:51:26 +0800

    App: Added Moving Background
    README: Minor update on Reflection

--- project/src/main/kotlin/App.kt
+++ project/src/main/kotlin/App.kt
@@ -1,83 +1,107 @@
 
 import kotlinx.css.*
 import kotlinx.css.properties.*
-
-
 import kotlinx.css.properties.IterationCount.Companion.infinite
-
-
+import org.w3c.dom.svg.SVGLinearGradientElement
 import react.RComponent
 import react.RProps
 import react.RState
 import react.RBuilder
+import react.dom.html
+import styled.*
 
-import styled.css
-import styled.keyframes
-import styled.keyframesName
-import styled.styledDiv
 import kotlin.random.Random
 
+const val TOP = 2000
 
 class App:RComponent<RProps,RState>() {
 
-private val boxShadows: BoxShadows by lazy { getBoxShadows(1000)}
+private val boxShadowsSmall: BoxShadows by lazy { getBoxShadows(500, Color.antiqueWhite)}
+    private val boxShadowsMedium: BoxShadows by lazy { getBoxShadows(100, Color.white)}
+
 
     override fun RBuilder.render() {
-        styledDiv{
 
+        styledDiv{
             styledDiv{
                 css {
                     width= 2.px
                     height= 2.px
                     borderRadius= 50.pct
                     background= "transparent"
-                    boxShadow = boxShadows
-                    animation("animStar", 150.s, Timing.linear,iterationCount = infinite)
+                    boxShadow = boxShadowsSmall
+                   animation("animStar", 250.s, Timing.linear,iterationCount = infinite)
                         after {
                             content= QuotedString(" ")
                             position=Position.absolute
-                            top=2000.px
-                            width= 2.px
-                            height= 2.px
+                            top=TOP.px
+                            width= 1.5.px
+                            height= 1.5.px
                             borderRadius = 50.pct
                             background = "transparent"
-                            boxShadow=boxShadows
+                            boxShadow=boxShadowsSmall
                         }
-
                 }
             }
             styledDiv{
+                css{
+                    width= 3.px
+                    height= 3.px
+                    borderRadius= 50.pct
+                    background= "transparent"
+                    boxShadow = boxShadowsMedium
+                    animation("animStar", 400.s, Timing.linear,iterationCount = infinite)
+                    after {
+                        content= QuotedString(" ")
+                        position=Position.absolute
+                        top=TOP.px
+                        width= 3.px
+                        height= 3.px
+                        borderRadius = 50.pct
+                        background = "transparent"
+                        boxShadow=boxShadowsMedium
+                    }
+
+                }
 
             }
             styledDiv{
 
             }
 
-//            css{
-//             keyframes("animStar",{
-//                 from { transform { translateY((-2000).px) } }
-//                 to{transform { translateY(0.px) }}
-//             })
-//            }
-        }
 
-
+            /* For Animation*/
+            injectGlobal("  @keyframes animStar {\n" +
+                    "    from {\n" +
+                    "      transform: translateY(0px);\n" +
+                    "    }\n" +
+                    "  \n" +
+                    "    to {\n" +
+                    "      transform: translateY(${-TOP}px);\n" +
+                    "    }\n" +
+                    "  }")
+          }
 
 
     }
+
+
+
+
+
 
     private fun getRandomInt(until:Int):LinearDimension= Random.nextInt(until).px
 
 
 
-    private fun getBoxShadows(n:Int): BoxShadows {
+    private fun getBoxShadows(n:Int,color:Color): BoxShadows {
         val boxShadows = BoxShadows()
         for (i in  0..n){
             boxShadows.plusAssign(
                 BoxShadow(false,
                     getRandomInt(2000),
                     getRandomInt(2000),
-                    0.px,1.px, Color.red)
+                    0.px,1.px, color)
             )
         }
         return boxShadows
--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,17 +1,45 @@
 import kotlinx.browser.document
 import kotlinx.css.*
-import react.dom.link
+import kotlinx.css.properties.transform
+import kotlinx.css.properties.translate
+import react.dom.h1
 import react.dom.render
+import styled.css
 import styled.injectGlobal
+import styled.styledH1
 
 
 fun main() {
+    injectGlobal(" html {\n" +
+            "  height: 100%;\n" +
+            "  background: radial-gradient(ellipse at bottom, #37577d 30%, #162157 100%);\n" +
+            "  overflow: hidden;\n" +
+            "}")
 
 
     render(document.getElementById("root")) {
+
+        styledH1{
+            +"Hi! I am Kalai."
+
+            css{
+                color= Color.whiteSmoke
+                fontFamily= "Courier New"
+                position = Position.absolute
+                top=40.pct
+                left=0.px
+                right=0.px
+                textAlign=TextAlign.center
+
+            }
+        }
         child(App::class) {}
+
+
     }
+
+
 }
 
-//  injectGlobal(string)
+
 
